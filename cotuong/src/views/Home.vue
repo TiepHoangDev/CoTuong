f4ec4645d4f1c15387a99e079e6964fa77be4f0a
TiepHp <TiepHp@MSI-TIEPHP>
2022-02-08 00:51:53 +0700

    start with pixijs

--- cotuong/src/views/Home.vue
+++ cotuong/src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div class="home">hello pixijs</div>
+    <canvas id="pixi-canvas"></canvas>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import * as PIXI from "pixi.js";
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  setup() {
+    return {};
   },
-})
-export default class Home extends Vue {}
+
+  methods: {
+    // demo() {
+    //   const app = new Application({
+    //     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+    //     resolution: window.devicePixelRatio || 1,
+    //     autoDensity: true,
+    //     backgroundColor: 0x6495ed,
+    //     width: 640,
+    //     height: 480,
+    //   });
+
+    //   const clampy: Sprite = Sprite.from("favicon.ico");
+
+    //   clampy.anchor.set(0.5);
+
+    //   clampy.x = app.screen.width / 2;
+    //   clampy.y = app.screen.height / 2;
+
+    //   app.stage.addChild(clampy);
+    // },
+
+    demo2() {
+      // Create the application helper and add its render target to the page
+      let app = new PIXI.Application({ width: 640, height: 360 });
+      document.body.appendChild(app.view);
+
+      // Create the sprite and add it to the stage
+      let sprite = PIXI.Sprite.from("favicon.ico");
+      app.stage.addChild(sprite);
+
+      // Add a ticker callback to move the sprite back and forth
+      let elapsed = 0.0;
+      app.ticker.add((delta) => {
+        elapsed += delta;
+        sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
+      });
+    },
+  },
+
+  mounted() {
+    this.demo2();
+  },
+});
 </script>
